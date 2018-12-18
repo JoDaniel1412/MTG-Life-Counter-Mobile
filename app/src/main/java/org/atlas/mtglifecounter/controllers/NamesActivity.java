459e23233938e66341ad510ba4d7bdf40b35ac51
JoDaniel1412 <jodaniel1412@gmail.com>
2018-12-18 12:03:46 -0600

    NamesActivity now sets the names to the Players

--- app/src/main/java/org/atlas/mtglifecounter/controllers/NamesActivity.java
+++ app/src/main/java/org/atlas/mtglifecounter/controllers/NamesActivity.java
@@ -6,10 +6,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 
 import org.atlas.mtglifecounter.R;
+import org.atlas.mtglifecounter.game.Game;
+import org.atlas.mtglifecounter.game.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NamesActivity extends AppCompatActivity {
+
+    Game game = Game.getInstance();
+    EditText p1;
+    EditText p2;
+    EditText p3;
+    EditText p4;
+    EditText p5;
+    EditText p6;
+    List<EditText> names = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +33,31 @@ public class NamesActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_names);
+
+        p1 = findViewById(R.id.player_name_1);
+        p2 = findViewById(R.id.player_name_2);
+        p3 = findViewById(R.id.player_name_3);
+        p4 = findViewById(R.id.player_name_4);
+        p5 = findViewById(R.id.player_name_5);
+        p6 = findViewById(R.id.player_name_6);
+
+        names.add(p1);
+        names.add(p2);
+        names.add(p3);
+        names.add(p4);
+        names.add(p5);
+        names.add(p6);
     }
 
     public void pressed_continue(View view) {
+        // Sets the name for each player
+        int i = 0;
+        for (Player player : game.getPlayers()) {
+            String name = names.get(i).getText().toString();
+            player.setName(name);
+            i++;
+        }
+
         Intent animation = new Intent(this, SetupGameActivity.class);
         startActivity(animation);
     }
