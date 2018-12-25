381851460572f4b06293f36fa44a5d4d6b7601f3
JoDaniel1412 <jodaniel1412@gmail.com>
2018-12-24 20:49:48 -0600

    Style modified and added an info button in the MainActivity

--- app/src/main/java/org/atlas/mtglifecounter/controllers/MainActivity.java
+++ app/src/main/java/org/atlas/mtglifecounter/controllers/MainActivity.java
@@ -1,26 +1,34 @@
 package org.atlas.mtglifecounter.controllers;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.Constraints;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import org.atlas.mtglifecounter.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ConstraintLayout infoLayout;
+    private boolean inLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        infoLayout = findViewById(R.id.infoLayout);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN && !inLayout) {
             pressed_start();
         }
         return super.onTouchEvent(event);
@@ -30,5 +38,20 @@ public class MainActivity extends AppCompatActivity {
     public void pressed_start() {
         Intent animation = new Intent(this, PlayerSelectionActivity.class);
         startActivity(animation);
+    }
+
+    public void pressed_info(View view) {
+        inLayout = true;
+        infoLayout.setVisibility(View.VISIBLE);
+
+    }
+
+    public void pressed_quit_info(View view) {
+        inLayout = false;
+        infoLayout.setVisibility(View.INVISIBLE);
+    }
+
+    public void pressed_settings(View view) {
+
     }
 }
